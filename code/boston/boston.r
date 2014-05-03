7e34ac10326c272c10e446b76daaffa977825f9f
Wesley Brooks <brooks.wesley@gmail.com>
2014-05-03 08:16:47 -0500

    just some comments

--- code/boston/boston.r
+++ code/boston/boston.r
@@ -1,12 +1,16 @@
 require(lagr)
 require(sp)
 require(doMC)
-registerDoMC(cores=3)
+registerDoMC(cores=2)
 
 load('data/boston/boston.rda')
 boston = boston.c
 boston$CHAS = as.numeric(boston$CHAS)
 
-bw = lagr.sel(MEDV~CRIM+INDUS+CHAS+NOX+RM+AGE+DIS+RAD+TAX+PTRATIO+B+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, varselect.method="AICc", range=c(0,1), kernel=epanechnikov, tol.bw=0.01, bw.type='knn', bwselect.method="AICc", parallel=TRUE, interact=TRUE, verbose=TRUE, family='gaussian', resid.type='pearson')
-#bw = 0.3
-ml = gwglmnet(MEDV~CRIM+INDUS+CHAS+NOX+RM+AGE+DIS+RAD+TAX+PTRATIO+B+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, N=1, varselect.method='AICc', kernel=epanechnikov, bw=bw[['bw']], bw.type='knn', parallel=TRUE, interact=TRUE, verbose=TRUE, family='gaussian', resid.type='pearson', simulation=TRUE)
+bw = list()
+#bw = lagr.sel(MEDV~CRIM+INDUS+CHAS+NOX+RM+AGE+DIS+RAD+TAX+PTRATIO+B+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, varselect.method="AICc", range=c(0,1), kernel=epanechnikov, tol.bw=0.01, bw.type='knn', bwselect.method="AICc", parallel=TRUE, interact=TRUE, verbose=TRUE, family='gaussian', resid.type='pearson')
+
+bw[['bw']] = 0.3
+Rprof("~/Desktop/boston-profile.txt")
+ml = lagr(MEDV~CRIM+INDUS+CHAS+NOX+RM+AGE+DIS+RAD+TAX+PTRATIO+B+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, N=1, varselect.method='AICc', kernel=epanechnikov, bw=bw[['bw']], bw.type='knn', parallel=FALSE, interact=TRUE, verbose=TRUE, family='gaussian', resid.type='pearson', simulation=TRUE)
+Rprof(NULL)
