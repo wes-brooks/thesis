6d2fdb44b160f07ddeda7f894fe3f63b40ab90c5
Wesley Brooks <brooks.wesley@gmail.com>
2014-05-09 18:57:06 -0500

    nightly - I've been playing with the boston code

--- code/boston/boston.r
+++ code/boston/boston.r
@@ -1,16 +1,18 @@
 require(lagr)
 require(sp)
 require(doMC)
-registerDoMC(cores=3)
+registerDoMC(cores=1)
 
-load('data/boston/boston.rda')
+#The data is from package 'spdep'
+require(spdep)
+data(boston)
 boston = boston.c
 boston$CHAS = as.numeric(boston$CHAS)
 
 bw.boston = list()
-bw.boston = lagr.sel(MEDV~CRIM+RM+RAD+TAX+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, varselect.method="AICc", range=c(0,1), kernel=epanechnikov, tol.bw=0.01, bw.type='knn', bwselect.method="AICc", parallel=TRUE, interact=TRUE, verbose=TRUE, family='gaussian', resid.type='pearson')
+bw.boston = lagr.sel(MEDV~CRIM+RM+RAD+TAX+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, varselect.method="AICc", range=c(0,.15), kernel=epanechnikov, tol.bw=0.01, bw.type='knn', bwselect.method="AICc", verbose=TRUE, family='gaussian', resid.type='pearson')
 
-#bw.boston[['bw']] = 0.2
+bw.boston[['bw']] = 0.05
 #Rprof("~/Desktop/boston-profile.txt")
-m.boston = lagr(MEDV~CRIM+RM+RAD+TAX+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], longlat=TRUE, N=1, varselect.method='AICc', kernel=epanechnikov, bw=bw.boston[['bw']], bw.type='knn', parallel=FALSE, interact=TRUE, verbose=TRUE, family='gaussian', resid.type='pearson', simulation=TRUE)
+m.boston = lagr(MEDV~CRIM+RM+RAD+TAX+LSTAT-1, data=boston, coords=boston[,c('LON','LAT')], fit.loc=boston[1,c("LON","LAT")], longlat=TRUE, varselect.method='AICc', kernel=epanechnikov, bw=bw.boston[['bw']], bw.type='knn', verbose=TRUE, family='gaussian', resid.type='pearson')
 #Rprof(NULL)
