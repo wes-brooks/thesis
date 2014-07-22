8b505dfebebf2bf572e3378b583dd74d3d05c8ee
Wesley Brooks <brooks.wesley@gmail.com>
2014-07-22 18:38:27 -0500

    nightly

--- writeup/estimation/use-template.rb
+++ writeup/estimation/use-template.rb
@@ -5,23 +5,40 @@ destination = "estimation.tex"
 template = "template.tex"
 
 estimation = []
+proofs = []
 body = false
+appendix = false
 
 #Read the source file
 File.open(source, "r").each_line do |line|
-    if (!body and line.include?("\\section{Introduction}"))
+    if (!body and !appendix and line.include?("\\section{Introduction}"))
         body = true
         puts "starting body"
     end
 
     if body
-        if line.include?("\\bibliographystyle") 
-            body = false
-            puts "ending body"
-        else
-            estimation << line
+        if not line == "\n"
+            if line.include?("\\section*{Appendix") 
+                body = false
+                appendix = true
+                puts "ending body, beginning appendix"
+            else
+                estimation << line
+            end
         end
     end
+
+    if appendix
+        if not line == "\n"
+            if line.include?("\\bibliographystyle") 
+                appendix = false
+                puts "ending appendix"
+            else
+                proofs << line
+            end
+        end
+    end
+
 end
 
 
@@ -32,10 +49,13 @@ File.open(template, "r").each_line do |line|
 end
 
 
-#Replace the content section in template:
-loc = template_contents.index {|s| s.include?("[content]")}
+#Replace the body section in template:
+loc = template_contents.index {|s| s.include?("[body]")}
 template_contents[loc..loc] = estimation
 
+#Replace the appendix section in template:
+loc = template_contents.index {|s| s.include?("[appendix]")}
+template_contents[loc..loc] = proofs
 
 #Write the complete result to disk
 File.open(destination, "w") do |f|
