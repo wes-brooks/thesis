25944537deed0344f5804ef4aed10224e3c87246
Wesley Brooks <brooks.wesley@gmail.com>
2014-08-18 23:36:22 -0500

    I think the edits are complete.

--- writeup/estimation/compile.sh
+++ writeup/estimation/compile.sh
@@ -1,13 +1,21 @@
 #! /bin/bash
 mkdir -p aux
+mkdir -p aux-supplement
 
-lyx --export pdflatex estimation.lyx
+#lyx --export pdflatex estimation.lyx
 
-ruby use-template.rb
-
+#Compile the main document:
+ruby use-template.rb --source=fromLyx.tex --destination=estimation.tex --template=template.tex
 xelatex -output-directory=aux estimation
 bibtex aux/estimation
 xelatex -output-directory=aux estimation
 xelatex -output-directory=aux estimation
-
 mv aux/estimation.pdf ./
+
+#Compile the supplement
+ruby use-template.rb --source=rawSupplement.tex --destination=supplement.tex --template=template.tex
+xelatex -output-directory=aux-supplement supplement
+bibtex aux-supplement/supplement
+xelatex -output-directory=aux-supplement supplement
+xelatex -output-directory=aux-supplement supplement
+mv aux-supplement/supplement.pdf ./
