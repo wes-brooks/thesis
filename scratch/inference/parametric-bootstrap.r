d75d1e65654f02d8a377f778243799562aea9c57
Wesley Brooks <brooks.wesley@gmail.com>
2014-11-12 21:36:43 -0600

    working on writing up inference

--- scratch/inference/parametric-bootstrap.r
+++ scratch/inference/parametric-bootstrap.r
@@ -12,7 +12,7 @@ bw$trace[order(bw$trace[,1]),][,c(1,2)] -> trace
 bw.range = range(trace[trace[,2]<min(trace[,2])+10,1])
 
 #Number of bootstrap draws:
-B = 20
+B = 25
 
 bootstrap.data = sim
 hh = seq(bw.range[1], bw.range[2], length.out=10)
@@ -20,9 +20,13 @@ for (h in hh) {
     cc = list()
     model = lagr(Y~X1+X2+X3+X4, data=sim, family='gaussian', coords=c('loc.x','loc.y'), longlat=FALSE, varselect.method='AIC', bw=h, kernel=epanechnikov, bw.type='knn', verbose=FALSE, n.lambda=100, lagr.convergence.tol=0.005)
     
+    bootstrap.seed = sapply(1:B, function(m) return(matrix(rnorm(15, 0, 1))))
     bootstrap.response = matrix(NA, 0, B)
     for (i in 1:nrow(sim)) {
-        bootstrap.coefs = mvrnorm(B, mu=model[['model']][[i]][['model']][['LS.coefs']][1:5], Sigma=4/3/pi * model[['model']][[i]][['model']][['results']][['full.model.cov']][1:5,1:5])
+        mu = model[['model']][[i]][['model']][['LS.coefs']][1:5]
+        Sigma = 4/3/pi * model[['model']][[i]][['model']][['results']][['full.model.cov']]
+        S = sqrtm(Sigma)
+        bootstrap.coefs = t((S[1:5,] %*% bootstrap.seed) + mu)
         cc[[i]] = bootstrap.coefs
         eta = bootstrap.coefs %*% matrix(as.numeric(cbind(1, sim[i,2:5])))
         bootstrap.response = rbind(bootstrap.response,
@@ -30,10 +34,11 @@ for (h in hh) {
     }
 
     bootstrap.model = list()
-    for (i in 1:20) {
+    fl = sim[133,c('loc.x', 'loc.y')]
+    for (i in 1:B) {
         cat(paste(i, "\n", sep=""))
         bootstrap.data$Y = bootstrap.response[,i]
-        bootstrap.model[[i]] = lagr(Y~X1+X2+X3+X4, data=bootstrap.data, family='gaussian', coords=c('loc.x','loc.y'), longlat=FALSE, varselect.method='AIC', bw=h, kernel=epanechnikov, bw.type='knn', verbose=FALSE, n.lambda=100, lagr.convergence.tol=0.005)
+        bootstrap.model[[i]] = lagr(Y~X1+X2+X3+X4, data=bootstrap.data, family='gaussian', coords=c('loc.x','loc.y'), fit.loc=fl, longlat=FALSE, varselect.method='AIC', bw=h, kernel=epanechnikov, bw.type='knn', verbose=FALSE, n.lambda=100, lagr.convergence.tol=0.005)
     }
     
     is.zero = matrix(NA,5,0)
@@ -46,7 +51,7 @@ for (h in hh) {
         coef.distribution = cbind(coef.distribution, as.double(coefs %*% exp(0.5*(min(crit)-crit)) / sum(exp(0.5*(min(crit)-crit)))))
     }
     
-    #Following the notation of Efron (2014):
-    V = 4/3/pi * model[['model']][[i]][['model']][['results']][['full.model.cov']][1:5,1:5]
-    cov(t(coef.distribution), cc[[1]])
+    #Following Efron (2014):
+    V = 4/3/pi * model[['model']][[133]][['model']][['results']][['full.model.cov']][1:5,1:5]
+    cov(t(coef.distribution), cc[[133]])
 }
