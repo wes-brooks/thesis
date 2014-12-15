deeb4d674619601bec8f4e61e2d7e1fd3118042d
Wesley Brooks <brooks.wesley@gmail.com>
2014-12-15 17:57:11 -0600

    nightly

--- writeup/inference-2/compile.sh
+++ writeup/inference-2/compile.sh
@@ -3,7 +3,7 @@
 mkdir -p aux
 
 pdflatex -output-directory=aux inference 
-biblatex -output-directory=aux inference
+bibtex aux/inference
 pdflatex -output-directory=aux inference
 pdflatex -output-directory=aux inference
 
